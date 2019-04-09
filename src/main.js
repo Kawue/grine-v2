edd40db1ef43433f956800aa049448c5f4071a23
Kim Wuestkamp <kim@wuestkamp.com>
2019-04-09 13:07:05 +0200

    vue create full

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import App from './App.vue'
-import BootstrapVue from 'bootstrap-vue'
-
-Vue.use(BootstrapVue);
-Vue.use(Vuex);
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app');
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
