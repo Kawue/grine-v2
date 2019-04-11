7488027d7c3d7587ef3facc549e146099214112c
Kim Wuestkamp <kim@wuestkamp.com>
2019-04-11 11:24:36 +0200

    created simple images component

---
+++ src/components/Images.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="images">
+    <h3>TEST</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Images',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+h3 {
+  margin: 140px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
