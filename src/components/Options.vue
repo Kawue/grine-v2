e910e435a48efc33426ebef0139c7f0c77e3fb85
Kim Wuestkamp <kim@wuestkamp.com>
2019-05-12 18:45:52 +0200

    icon toggles option tabs

--- src/components/Options.vue
+++ src/components/Options.vue
@@ -5,29 +5,35 @@
     @mouseleave="mouseLeave"
   >
     <div class="options-nav float-left">
-      <div
-        @mouseover="mouseTabOver('network')"
-        @mousedown="mouseTabDown('network')"
-        v-bind:class="getTabClass('network')"
-        class="options-nav-btn"
-      >
-        <p>Network</p>
+      <div @click="tabsExpanded = !tabsExpanded" class="options-tab-toggle" v-bind:class="{ active: tabsExpanded }">
+        <img src="images/options.svg" />
       </div>
-      <div
-        @mouseover="mouseTabOver('image')"
-        @mousedown="mouseTabDown('image')"
-        v-bind:class="getTabClass('image')"
-        class="options-nav-btn"
-      >
-        <p>Image</p>
-      </div>
-      <div
-        @mouseover="mouseTabOver('data')"
-        @mousedown="mouseTabDown('data')"
-        v-bind:class="getTabClass('data')"
-        class="options-nav-btn"
-      >
-        <p>Data</p>
+
+      <div v-if="tabsExpanded">
+        <div
+          @mouseover="mouseTabOver('network')"
+          @mousedown="mouseTabDown('network')"
+          v-bind:class="getTabClass('network')"
+          class="options-nav-tab"
+        >
+          <p>Network</p>
+        </div>
+        <div
+          @mouseover="mouseTabOver('image')"
+          @mousedown="mouseTabDown('image')"
+          v-bind:class="getTabClass('image')"
+          class="options-nav-tab"
+        >
+          <p>Image</p>
+        </div>
+        <div
+          @mouseover="mouseTabOver('data')"
+          @mousedown="mouseTabDown('data')"
+          v-bind:class="getTabClass('data')"
+          class="options-nav-tab"
+        >
+          <p>Data</p>
+        </div>
       </div>
     </div>
 
@@ -64,6 +70,7 @@ export default {
     return {
       tabActive: null,
       tabLocked: null,
+      tabsExpanded: false,
     };
   },
   methods: {
@@ -85,7 +92,7 @@ export default {
       }
     },
     showOptionsContent: function() {
-      return this.tabActive !== null;
+      return this.tabActive !== null && this.tabsExpanded;
     },
     getExpandedClass: function() {
       return this.showOptionsContent() ? 'expanded' : '';
@@ -116,7 +123,6 @@ export default {
   }
   label {
     font-size: 14px;
-    /*padding: 0;*/
     margin: 0;
   }
   .btn-secondary {
@@ -133,8 +139,7 @@ export default {
 
 <style scoped lang="scss">
 .options {
-  min-height: 450px;
-  max-height: 450px;
+  height: 490px;
   width: 40px;
   overflow: hidden;
   border-radius: 0 0 10px 0;
@@ -150,10 +155,35 @@ export default {
     color: white;
     padding: 10px 10px 10px 50px;
   }
+  .options-tab-toggle {
+    width: 40px;
+    height: 40px;
+    background-color: #4f5051;
+    border: 1px solid #737374;
+    border-top: 0;
+    border-left: 0;
+    border-radius: 0 0 10px 0;
+    position: relative;
+    cursor: pointer;
+    &.active {
+      background-color: #3e3f40;
+    }
+
+    img {
+      width: 25px;
+      height: 25px;
+      position: absolute;
+      margin: auto;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+    }
+  }
 }
 
 .options-nav {
-  .options-nav-btn {
+  .options-nav-tab {
     &.locked {
       border: 1px solid white !important;
     }
@@ -169,10 +199,11 @@ export default {
     margin: 0 0 0 -1px;
     padding: 0;
     cursor: pointer;
+    color: white;
     background-color: #4f5051;
     border: 1px solid #737374;
-    color: white;
     border-top: 0;
+    border-left: 0;
     border-radius: 0 10px 10px 0;
 
     p {
