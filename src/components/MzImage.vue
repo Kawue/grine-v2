118db6f66b93e4ecaf2f7895b053f6461b8a2ead
Kim Wuestkamp <kim@wuestkamp.com>
2019-05-23 15:00:44 +0200

    simple heatmap test

--- src/components/MzImage.vue
+++ src/components/MzImage.vue
@@ -2,11 +2,20 @@
   <div>
     <svg width="250" height="250">
       <g style="transform: translate(0, 10px)">
-        <circle
+        <!--<circle-->
+          <!--v-for="point in points"-->
+          <!--r="1"-->
+          <!--v-bind:style="pointStyle(point)"-->
+        <!--&gt;</circle>-->
+        <rect
           v-for="point in points"
           r="1"
-          v-bind:style="pointStyle(point)"
-        ></circle>
+          v-bind:x="point[0]"
+          v-bind:y="point[1]"
+          width="10"
+          height="10"
+          v-bind:fill="pointColor(point[2])"
+        ></rect>
       </g>
     </svg>
   </div>
@@ -19,16 +28,29 @@ export default {
   name: 'MzImage',
   data() {
     return {
-      points: [[10, 10], [20, 20], [30, 30], [40, 40], [50, 50], [500, 500]],
+      points: [
+        /*[10, 10, 55],
+        [20, 20, 0],
+        [30, 30, 33],
+        [40, 40, 89],
+        [50, 50, 80],
+        [500, 500, 4],*/
+      ],
     };
   },
   mounted() {
-    this.calculatePath();
+    this.calculateImageData();
   },
   methods: {
-    pointStyle(point) {
-      return 'transform: translate(' + point[0] + 'px, ' + point[1] + 'px)';
+    pointColor(intensity) {
+      let myColor = d3.scaleLinear()
+        .range(['white', '#69b3a2'])
+        .domain([1, 100]);
+      return myColor(intensity);
     },
+    /*pointStyle(point) {
+      return 'transform: translate(' + point[0] + 'px, ' + point[1] + 'px)';
+    },*/
     /*getScales() {
       const x = d3.scaleTime().range([0, 430]);
       const y = d3.scaleLinear().range([210, 0]);
@@ -38,21 +60,13 @@ export default {
       y.domain([0, d3.max(this.data, d => d)]);
       return { x, y };
     },*/
-    calculatePath() {
-      const x = d3.scaleTime().range([0, 250]);
-      const y = d3.scaleLinear().range([250, 0]);
-
-      d3.axisLeft().scale(x);
-      d3.axisBottom().scale(y);
-
-      //console.log(scale.x(1));
-
-      /*const path = d3.line()
-        .x((d, i) => scale.x(i))
-        .y(d => scale.y(d));*/
-
-
-      //this.line = path(this.data);
+    calculateImageData() {
+      for (let i = 0; i < 10000; i++) {
+        let x = Math.floor(Math.random() * 250) + 0;
+        let y = Math.floor(Math.random() * 250) + 0;
+        let d = Math.floor(Math.random() * 100) + 0;
+        this.points.push([x, y, d]);
+      }
     },
   },
 };
