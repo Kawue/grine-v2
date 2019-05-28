d798040ae43c663926f89ed9e8a3f7b63f16b385
Kim Wuestkamp <kim@wuestkamp.com>
2019-05-28 12:41:31 +0200

    use canvas to draw instead of svg

--- src/components/MzImage.vue
+++ src/components/MzImage.vue
@@ -1,22 +1,28 @@
 <template>
   <div>
-    <svg v-bind:width="width" v-bind:height="height">
-      <g v-if="render">
-        <rect
-          v-for="point in points"
-          v-bind:x="getPosX(point)"
-          v-bind:y="getPosY(point)"
-          v-bind:width="getWidth()"
-          v-bind:height="getHeight()"
-          v-bind:fill="getPointColor(point)"
-        ></rect>
-      </g>
-    </svg>
+    <!--<svg v-bind:width="width" v-bind:height="height">-->
+      <!--<g v-if="render">-->
+        <!--<rect-->
+          <!--v-for="point in points"-->
+          <!--v-bind:x="getPosX(point)"-->
+          <!--v-bind:y="getPosY(point)"-->
+          <!--v-bind:width="getWidth()"-->
+          <!--v-bind:height="getHeight()"-->
+          <!--v-bind:fill="getPointColor(point)"-->
+        <!--&gt;</rect>-->
+      <!--</g>-->
+    <!--</svg>-->
+    <canvas
+      id="plot-canvas"
+      class="plot"
+      v-bind:width="width"
+      v-bind:height="height"
+    ></canvas>
     <button @click="clearImageData">Clear</button>
     <button @click="generateRandomImageData">Generate</button>
     <button @click="toggleRenderRandomImageData">Toggle Render</button>
 
-    <br>
+    <br />
     <p>Clear: clear the data array</p>
     <p>Generate: generate a new random data array</p>
     <p>Toggle Render: toggle boolean to render svg g-element via v-if</p>
@@ -73,6 +79,21 @@ export default {
     },
   },
   methods: {
+    drawCanvas() {
+      let c = document.getElementById('plot-canvas');
+      let ctx = c.getContext('2d');
+      ctx.clearRect(0, 0, c.width, c.height);
+      let that = this;
+      this.points.forEach(function(point) {
+        ctx.fillStyle = that.getPointColor(point);
+        ctx.fillRect(
+          that.getPosX(point),
+          that.getPosY(point),
+          that.getWidth(),
+          that.getHeight()
+        );
+      });
+    },
     clearImageData() {
       this.points = [];
     },
@@ -85,7 +106,8 @@ export default {
       }
     },
     toggleRenderRandomImageData() {
-      this.render = !this.render;
+      //this.render = !this.render;
+      this.drawCanvas();
     },
     getPointColor(point) {
       return this.pointColor(point[2]);
@@ -112,6 +134,10 @@ export default {
 
 <style lang="scss" scoped>
 svg {
+  margin: 25px;
+  border: 1px solid lightgrey;
+}
+canvas {
   margin: 25px;
   border: 1px solid lightgrey;
 }
