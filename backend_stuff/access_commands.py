1c961bfc23732ffd89a7ab39ac29596c10e74f0c
Kim Wuestkamp <kim@wuestkamp.com>
2019-06-05 15:06:13 +0200

    api working

--- backend_stuff/access_commands.py
+++ backend_stuff/access_commands.py
@@ -7,35 +7,54 @@ from flask import Flask
 # load dataframe (.h5)
 merged_dframe = pd.read_hdf(argv[1])
 
-dataset_names = set(merged_dframe.index.get_level_values("dataset"))
 
-# select on of the 3 data sets
+# select one of the 3 data sets
 example_ds_name = "barley101_1"
-single_dframe = merged_dframe.loc[merged_dframe.index.get_level_values("dataset") == example_ds_name]
-
-# get x positions for all pixels from a selected single data set
-pos_x = np.array(single_dframe.index.get_level_values("grid_x"))
-
-# get y positions for all pixels from a selected single data set
-pos_y = np.array(single_dframe.index.get_level_values("grid_y"))
-
-# select a single mz image vector from a selected single data set
-example_mz = 74.651
-
-print(single_dframe)
-
-print(pos_x)
-print(pos_y)
 
 
-print(single_dframe.index)
+# returns names of all available datasets
+def dataset_names():
+    dt_names = set(merged_dframe.index.get_level_values("dataset"))
 
+    names = []
+    for name in dt_names:
+        names.append(name)
+    return names
+
+
+# returns list of all mz_values
+def mz_values(ds_name):
+    single_dframe = merged_dframe.loc[merged_dframe.index.get_level_values("dataset") == ds_name]
+
+    mzs = []
+    for key, value in single_dframe.iteritems():
+        mzs.append(key)
+    return mzs
+
+
+# provides data to render image for passed mz_value
+def mz_image_data(ds_name, mz_value):
+    single_dframe = merged_dframe.loc[merged_dframe.index.get_level_values("dataset") == ds_name]
+
+    # get x positions for all pixels from a selected single data set
+    pos_x = np.array(single_dframe.index.get_level_values("grid_x"))
+
+    # get y positions for all pixels from a selected single data set
+    pos_y = np.array(single_dframe.index.get_level_values("grid_y"))
+
+    # select a single mz image vector from a selected single data set
+
+    intensity = list(np.array(single_dframe[mz_value]).astype(float))
+
+    return [{'x': int(x), 'y': int(y), 'intensity': float(i)} for x, y, i in zip(pos_x, pos_y, intensity)]
+
+
+#print(intensity)
+#print(test[:, 2])
+#print(single_dframe[example_mz])
+#print(single_dframe[example_mz][4])
 #intensity = np.array(single_dframe.index.get_level_values(example_mz))
 #print(intensity)
-
-
-
-
 #print(single_dframe[example_mz])
 
 
@@ -43,25 +62,18 @@ app = Flask(__name__)
 
 
 @app.route('/datasets')
-def datasets():
-    names = []
-    for name in dataset_names:
-      names.append(name)
-    return json.dumps(names)
+def datasets_action():
+    return json.dumps(dataset_names())
 
 
 @app.route('/mzvalues')
-def mzvalues():
-    mzs = []
-    for key, value in single_dframe.iteritems():
-        mzs.append(key)
-    return json.dumps(mzs)
+def mz_values_action():
+    return json.dumps(mz_values(dataset_names()[0]))
 
 
 @app.route('/mzdata')
-def mzdata():
-    return "test"
-    #return json.dumps(single_dframe[example_mz])
+def mz_data_action():
+    return json.dumps(mz_image_data(dataset_names()[0], 74.651))
 
 
 if __name__ == '__main__':
