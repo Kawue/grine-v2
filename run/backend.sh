10923298d7397d14910a72052130f5bb4184d9f4
Kim Wuestkamp <kim@wuestkamp.com>
2019-06-25 12:58:28 +0200

    use real data for python backend run

--- run/backend.sh
+++ run/backend.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 cd backend
-python3 api.py fake_data.json barley_randomized.h5
+#python3 api.py fake_data.json barley_randomized.h5
+python3 api.py real_data.json barley101GrineV2.h5
