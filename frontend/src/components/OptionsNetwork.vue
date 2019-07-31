bffa55f9215354c6ab2fe34999b642b8751db0cf
Daniel Göbel <dgoebel@techfak.uni-bielefeld.de>
2019-07-31 16:30:35 +0200

    Use slider instead of select for network options
    update bootstrap vue

--- frontend/src/components/OptionsNetwork.vue
+++ frontend/src/components/OptionsNetwork.vue
@@ -2,28 +2,28 @@
   <div class="network">
     <div class="row">
       <div class="col-md-12">
-        <b-form-select
-          v-on:change="updateParameters"
-          v-model="parameters.repulsion"
-          class="mb-3"
-          :options="repulsionOptions"
-        >
-        </b-form-select>
-        <b-form-select
-          v-on:change="updateParameters"
+        <div class="slider-label">Repulsion: {{ repulsion }}</div>
+        <vue-slider
+          v-model="repulsion"
+          v-bind="optionsRepulsionSlider"
+          @change="updateParameters"
+          class="slider"
+        ></vue-slider>
+        <div class="slider-label">Iterations: {{ parameters.iterations }}</div>
+        <vue-slider
           v-model="parameters.iterations"
-          class="mb-3"
-          :options="iterationOptions"
-        >
-        </b-form-select>
-        <b-form-select
-          v-on:change="updateParameters"
+          v-bind="optionsIterationSlider"
+          @change="updateParameters"
+          class="slider"
+        ></vue-slider>
+        <div class="slider-label">Edge Length: {{ parameters.edgeLength }}</div>
+        <vue-slider
           v-model="parameters.edgeLength"
-          class="mb-3"
-          :options="edgeLengthOptions"
-        >
-        </b-form-select>
-        <b-button variant="outline-danger" @click="clearList">Clear</b-button>
+          v-bind="optionsEdgelengthSlider"
+          @change="updateParameters"
+          class="slider"
+          style="margin-bottom: 20px !important"
+        ></vue-slider>
       </div>
     </div>
     <hr />
@@ -34,75 +34,57 @@
 import store from '@/store';
 import { mapGetters } from 'vuex';
 import * as _ from 'lodash';
+import VueSlider from 'vue-slider-component';
+import 'vue-slider-component/theme/default.css';
 
 export default {
+  components: {
+    VueSlider,
+  },
   name: 'OptionsNetwork',
   data() {
     return {
       parameters: {
-        repulsion: 0,
-        iterations: 0,
-        edgeLength: 0,
+        repulsion: 50,
+        iterations: 100,
+        edgeLength: 50,
       },
-      repulsionOptions: [
-        {
-          value: -50,
-          text: '50',
-        },
-        {
-          value: -150,
-          text: '150',
-        },
-        {
-          value: -300,
-          text: '300',
-        },
-        {
-          value: -500,
-          text: '500',
-        },
-      ],
-      iterationOptions: [
-        {
-          value: 200,
-          text: '200',
-        },
-        {
-          value: 300,
-          text: '300',
-        },
-        {
-          value: 500,
-          text: '500',
-        },
-        {
-          value: 1000,
-          text: '1000',
-        },
-      ],
-      edgeLengthOptions: [
-        {
-          value: 10,
-          text: '10',
-        },
-        {
-          value: 30,
-          text: '30',
-        },
-        {
-          value: 75,
-          text: '75',
-        },
-        {
-          value: 150,
-          text: '150',
-        },
-      ],
+      optionsRepulsionSlider: {
+        min: -100,
+        max: 500,
+        interval: 10,
+        lazy: true,
+        marks: val => (val + 100) % 120 === 0,
+      },
+      optionsEdgelengthSlider: {
+        min: 50,
+        max: 500,
+        interval: 10,
+        lazy: true,
+        marks: val => (val - 50) % 90 === 0,
+      },
+      optionsIterationSlider: {
+        min: 100,
+        max: 1000,
+        interval: 20,
+        lazy: true,
+        marks: val => (val - 100) % 180 === 0,
+      },
     };
   },
-  computed: mapGetters({
-    force: 'networkOptions',
-  }),
+  computed: {
+    ...mapGetters({
+      force: 'networkOptions',
+    }),
+    repulsion: {
+      get() {
+        return -this.parameters.repulsion;
+      },
+      set(value) {
+        this.parameters.repulsion = -value;
+      },
+    },
+  },
   methods: {
     clearList() {
       store.commit('MZLIST_RESET_HIGHLIGHTED_MZ');
@@ -113,26 +95,19 @@ export default {
   },
   mounted() {
     this.parameters = _.cloneDeep(this.force);
-    for (const o of this.repulsionOptions) {
-      if (o.value === this.force.repulsion) {
-        o['selected'] = true;
-      }
-    }
-    for (const o of this.iterationOptions) {
-      if (o.value === this.force.iterations) {
-        o['selected'] = true;
-      }
-    }
-    for (const o of this.edgeLengthOptions) {
-      if (o.value === this.force.edgeLength) {
-        o['selected'] = true;
-      }
-    }
+    this.repulsion = -this.force.repulsion;
   },
 };
 </script>
 
 <style scoped lang="scss">
 .network {
+}
+.slider-label {
+  margin-bottom: 10px;
+  font-size: 1.2em;
+}
+.slider {
+  margin: 0 15px 70px 15px;
 }
 </style>
