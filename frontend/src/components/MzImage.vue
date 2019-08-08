8e9f6e3bc45eff7ebf3f5cc95a51e47a4d471b51
Kim Wuestkamp <kim@wuestkamp.com>
2019-08-08 15:15:50 +0200

    lasso reset fix

--- frontend/src/components/MzImage.vue
+++ frontend/src/components/MzImage.vue
@@ -166,6 +166,9 @@ export default {
     },
     handleLassoStart() {
       this.removeLassoAfterPointsDrawn = false;
+      if (store.getters.isMzLassoSelectionActive) {
+        store.commit('RESET_SELECTION');
+      }
       store.dispatch('imagesSelectPoints', [this.imageDataIndex, []]);
     },
     drawPoints() {
