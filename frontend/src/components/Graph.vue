343a7f1374cb169908c210d9cdb345253d353e49
Daniel Göbel <dgoebel@techfak.uni-bielefeld.de>
2019-09-17 10:31:18 +0200

    Add confirm Modal for merging nodes/changing nodes assignments and restructure bottom bar

--- frontend/src/components/Graph.vue
+++ frontend/src/components/Graph.vue
@@ -7,11 +7,113 @@
       v-bind:height="this.height"
       class="graphd3"
     ></svg>
+
+    <!-- Modal for selection of assignment parent -->
+    <b-modal
+      id="assignmentModal"
+      ref="assignmentModal"
+      title="Select Parent for all"
+      @ok="handleOk"
+      @cancel="handleCancel"
+    >
+      <template slot="default">
+        <b-form-group style="margin-bottom: 0;">
+          <b-form-radio-group
+            v-model="selected"
+            name="radios-stacked"
+            class="radio-selection"
+            v-bind:style="{
+              maxHeight: 70 * Math.ceil(myList.length / 3) + 'px',
+            }"
+          >
+            <div
+              v-for="node in myList"
+              class="radio-selection-node"
+              v-bind:key="node.name"
+            >
+              <svg
+                width="50px"
+                height="50px"
+                style="margin-right: 10px"
+                @click="selected = parseInt(node.name.split('n')[1], 10)"
+              >
+                <circle :fill="node.color" cx="25" cy="25" r="25" />
+              </svg>
+              <b-form-radio :value="parseInt(node.name.split('n')[1], 10)">
+                {{ node.name }}</b-form-radio
+              >
+            </div>
+          </b-form-radio-group>
+        </b-form-group>
+      </template>
+      <template slot="modal-footer" slot-scope="{ cancel, ok }">
+        <b-button variant="danger" @click="testDelete">
+          Delete
+        </b-button>
+        <b-button variant="primary" @click="testAdd">
+          Add
+        </b-button>
+        <b-button variant="outline-danger" @click="cancel()">
+          Cancel
+        </b-button>
+        <b-button variant="success" @click="ok()" :disabled="!state">
+          Confirm
+        </b-button>
+      </template>
+    </b-modal>
+
+    <!-- Bottom Bar -->
+    <div id="bottom-button-container">
+      <b-button variant="warning" size="lg" @click="clearSelection"
+        >Clear</b-button
+      >
+
+      <b-button
+        class="large-left-margin"
+        variant="warning"
+        size="lg"
+        :disabled="!(nodeTrixActive && !lassoActive)"
+        @click="resetNodeTrix"
+        >Reset NodeTrix</b-button
+      >
+      <b-button
+        class="small-left-margin"
+        variant="primary"
+        size="lg"
+        :disabled="!(nodeTrixPossible && !lassoActive)"
+        @click="computeNodeTrix"
+        >NodeTrix</b-button
+      >
+
+      <b-button
+        class="large-left-margin"
+        variant="primary"
+        size="lg"
+        :disabled="!splitPossible"
+        @click="splitCluster"
+        >Split</b-button
+      >
+      <b-button
+        v-b-modal="'assignmentModal'"
+        variant="primary"
+        size="lg"
+        class="small-left-margin"
+        >Change Assignment</b-button
+      >
+      <b-button
+        v-b-modal="'assignmentModal'"
+        variant="primary"
+        size="lg"
+        class="small-left-margin"
+        >Merge Nodes</b-button
+      >
+    </div>
   </div>
 </template>
 
 <script>
 import store from '@/store';
+import * as d3 from 'd3';
 import { mapGetters } from 'vuex';
 
 export default {
@@ -20,11 +122,61 @@ export default {
     return {
       height: window.innerHeight,
       width: window.innerWidth,
+      selected: null,
+      myList: [
+        { name: 'h0n11', color: '#AAFFAA' },
+        { name: 'h0n12', color: '#ff433f' },
+      ],
+      counter: 13,
     };
   },
-  computed: mapGetters({
-    networkSvg: 'networkSVGElements',
-  }),
+  computed: {
+    ...mapGetters({
+      networkSvg: 'networkSVGElements',
+      nodeTrixPossible: 'networkNodeTrixPossible',
+      nodeTrixActive: 'networkNodeTrixActive',
+      lassoActive: 'isMzLassoSelectionActive',
+      splitPossible: 'networkClusterSplitPossible',
+    }),
+    state() {
+      return Boolean(this.selected);
+    },
+  },
+  methods: {
+    clearSelection() {
+      store.commit('RESET_SELECTION');
+    },
+    computeNodeTrix() {
+      store.commit('NETWORK_COMPUTE_NODETRIX');
+    },
+    resetNodeTrix() {
+      store.commit('NETWORK_NODETRIX_RESET');
+    },
+    splitCluster() {
+      store.commit('NETWORK_SPLIT_CLUSTER');
+    },
+    handleOk() {
+      console.log('Modal Ok');
+      console.log(this.selected);
+      this.selected = null;
+    },
+    handleCancel() {
+      console.log('Modal cancel');
+      this.selected = null;
+    },
+    testAdd() {
+      this.myList.push({
+        name: 'h0n' + this.counter,
+        color: d3.interpolateRainbow(Math.random()),
+      });
+      this.counter++;
+    },
+    testDelete() {
+      if (this.myList.length > 2) {
+        this.myList.splice(0, 1);
+      }
+    },
+  },
   mounted() {
     store.commit('NETWORK_LOAD_GRAPH');
     store.commit('NETWORK_INIT_SVG');
@@ -52,6 +204,13 @@ export default {
   z-index: 100;
   color: white;
 }
+#bottom-button-container {
+  position: absolute;
+  left: 3vw;
+  bottom: 5vh;
+  z-index: 100;
+  font-size: 1.2em;
+}
 .lasso path {
   stroke: rgb(239, 235, 220);
   stroke-width: 2px;
@@ -66,5 +225,23 @@ export default {
 .lasso .origin {
   fill: $primary;
   fill-opacity: 0.5;
+}
+.custom-control {
+  display: inline-block;
+}
+.radio-selection {
+  display: flex;
+  flex-flow: row wrap;
+  align-content: space-evenly;
+}
+.radio-selection-node {
+  margin: 10px 5px;
+  display: inline-block;
+}
+.small-left-margin {
+  margin-left: 15px;
+}
+.large-left-margin {
+  margin-left: 50px;
 }
 </style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,47 +3,8 @@
     <SidebarLeft />
     <SidebarRight v-if="!loading" />
     <Graph v-if="!loading" />
-    <div id="bottom-button-container">
-      <b-button
-        id="clear-button"
-        variant="warning"
-        size="lg"
-        @click="clearSelection"
-        >Clear</b-button
-      >
-
-      <b-button
-        variant="warning"
-        size="lg"
-        :disabled="!(nodeTrixActive && !lassoActive)"
-        @click="resetNodeTrix"
-        >Reset NodeTrix</b-button
-      >
-      <b-button
-        id="nodeTrix-button"
-        variant="primary"
-        size="lg"
-        :disabled="!(nodeTrixPossible && !lassoActive)"
-        @click="computeNodeTrix"
-        >NodeTrix</b-button
-      >
-
-      <b-button
-        style="margin-left: 50px;"
-        variant="primary"
-        size="lg"
-        v-if="splitPossible"
-        @click="splitCluster"
-        >Split</b-button
-      >
-    </div>
     <div class="mode-container text-center" @click="toggleMode">
-      <div v-if="lassoMode" class="selected-mode">
-        Free Mode
-      </div>
-      <div v-else class="selected-mode">
-        Lasso Mode
-      </div>
+      <div class="selected-mode">{{ lassoMode ? 'Free' : 'Lasso' }} Mode</div>
     </div>
   </div>
 </template>
@@ -66,10 +27,6 @@ export default {
     ...mapGetters({
       loading: 'getLoadingGraphData',
       lassoMode: 'networkLassoMode',
-      nodeTrixPossible: 'networkNodeTrixPossible',
-      nodeTrixActive: 'networkNodeTrixActive',
-      lassoActive: 'isMzLassoSelectionActive',
-      splitPossible: 'networkClusterSplitPossible',
     }),
   },
   mounted: function() {
@@ -77,28 +34,13 @@ export default {
     store.dispatch('fetchMergeMethods');
   },
   methods: {
-    clearSelection() {
-      store.commit('RESET_SELECTION');
-    },
-    computeNodeTrix() {
-      store.commit('NETWORK_COMPUTE_NODETRIX');
-    },
-    resetNodeTrix() {
-      store.commit('NETWORK_NODETRIX_RESET');
-    },
     toggleMode() {
       store.commit('NETWORK_TOGGLE_MODE');
-    },
-    splitCluster() {
-      store.commit('NETWORK_SPLIT_CLUSTER');
     },
   },
 };
 </script>
 <style scoped lang="scss">
-#clear-button {
-  margin-right: 50px;
-}
 .selected-mode {
   width: 150px;
   font-size: 1.4em;
@@ -112,15 +54,5 @@ export default {
   top: 0;
   z-index: 100;
   cursor: pointer;
-}
-#nodeTrix-button {
-  margin-left: 15px;
-}
-#bottom-button-container {
-  position: absolute;
-  left: 3vw;
-  bottom: 5vh;
-  z-index: 100;
-  font-size: 1.2em;
 }
 </style>
