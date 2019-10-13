d8ea55113dd761a51f2e27ca73a0a9d27166cc8b
Daniel Göbel <dgoebel@techfak.uni-bielefeld.de>
2019-10-13 14:31:59 +0200

    Update README and startup script

--- run/backend.sh
+++ run/backend.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 cd backend
-# python3 api.py fake_data.json barley_randomized.h5
-# python3 api.py real_data.json barley101GrineV2.h5
-# python3 api.py real_data.json kidneyGrineV2.h5
-# python3 api.py real_data.json vibrissaeGrineV2.h5
 python3 api.py real_data.json dimreduce_example.h5 barley101GrineV2.h5
